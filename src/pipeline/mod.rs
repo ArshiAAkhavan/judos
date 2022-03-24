507d1f7ad0465eea549058e88c27e7d1776757c1
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-03-25 01:05:10 +0430

    compiles

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -4,7 +4,7 @@ mod scoreboard;
 mod stage;
 use std::{thread, time};
 
-use crossbeam::{channel, select};
+use crossbeam::{self, channel, select};
 use serde::Deserialize;
 
 //TODO: better place for CommitHash Type;
@@ -45,59 +45,58 @@ impl Pipeline {
         // TODO: handle exit signal
         let (_stx, srx) = channel::bounded::<()>(self.concurrency + 1);
 
-        let mut handles = Vec::new();
         // poll_all thread
         let srx_pollall = srx.clone();
-        handles.push(thread::spawn(move || {
-            let interval = channel::tick(time::Duration::from_secs(self.poll_interval as u64));
-            loop {
-                select! {
-                    recv(interval) -> _ticked => {
-                        for repo in &self.repos{
-                            for stage in self.stages{
-                                ptx.send(Work::new(GitTarget::repo(repo.clone()),&stage));
+        crossbeam::scope(|s| {
+            s.spawn(|_| {
+                let interval = channel::tick(time::Duration::from_secs(self.poll_interval as u64));
+                loop {
+                    select! {
+                        recv(interval) -> _ticked => {
+                            for repo in &self.repos{
+                                for stage in &self.stages{
+                                    ptx.send(Work::new(GitTarget::repo(repo.clone()),&stage)).unwrap();
+                                }
+                            }
+                        },
+                        recv(srx_pollall) -> _sig => {
+                            eprintln!("poll_all thread recieved exit signal, exiting")
+                        }
+                    }
+                }
+            });
+            for i in 1..=self.concurrency {
+                let srx = srx.clone();
+                let prx = prx.clone();
+                let (wtx, wrx) = (wtx.clone(), wrx.clone());
+                s.spawn(move |_| loop {
+                    select! {
+                        recv(srx) -> _sig => {
+                            eprintln!("thread {i} recieved exit signal, exiting...");
+                            return;
+                        },
+                        recv(prx) -> work => {
+                            let Work { target, stage } = work.unwrap();
+                            // TODO: check for duplicate polled
+                            match stage.poll(target) {
+                                Some(target) => wtx.send(Work::new(target, stage)).unwrap(),
+                                None => (),
                             }
                         }
-                    },
-                    recv(srx_pollall) -> _sig => {
-                        eprintln!("poll_all thread recieved exit signal, exiting")
+                        recv(wrx) -> work => {
+                            let Work { target, stage } = work.unwrap();
+                            match stage.trigger(&target) {
+                                Ok(grade) => {
+                                    self.scoreboard.update_grade(&stage.name, &target, grade);
+                                },
+                                // TODO: better logging
+                                Err(e) => {eprintln!("{e:?}")},
+                            }
+                        }
                     }
-                }
+                });
             }
-        }));
-        for i in 1..=self.concurrency {
-            let srx = srx.clone();
-            let prx = prx.clone();
-            let (wtx, wrx) = (wtx.clone(), wrx.clone());
-            handles.push(thread::spawn(move || loop {
-                select! {
-                    recv(srx) -> _sig => {
-                        eprintln!("thread {i} recieved exit signal, exiting...");
-                        return;
-                    },
-                    recv(prx) -> work => {
-                        let Work { target, stage } = work.unwrap();
-                        // TODO: check for duplicate polled
-                        match stage.poll(target) {
-                            Some(target) => wtx.send(Work::new(target, stage)).unwrap(),
-                            None => (),
-                        }
-                    }
-                    recv(wrx) -> work => {
-                        let Work { target, stage } = work.unwrap();
-                        match stage.trigger(&target) {
-                            Ok(grade) => {
-                                self.scoreboard.update_grade(&stage.name, &target, grade);
-                            },
-                            // TODO: better logging
-                            Err(e) => {eprintln!("{e:?}")},
-                        }
-                    }
-                }
-            }));
-        }
-        for h in handles {
-            h.join();
-        }
+        })
+        .unwrap();
     }
 }
