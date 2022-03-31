c3698163bc4e264be42812b8ab75cf733fe7d012
ArshiAAkhavan <letmemakenewone@gmail.com>
2022-03-31 13:55:09 +0430

    better error handleing

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use crossbeam::channel;
+use env_logger;
 use judos::pipeline::Pipeline;
 use log::info;
-use env_logger;
-use crossbeam::channel;
 fn main() {
     env_logger::init();
     info!("rendering config files");
     let config_file = std::fs::File::open("./config.yml").unwrap();
     let p: Pipeline = serde_yaml::from_reader(config_file).unwrap();
     info!("running...");
-    let (stx,srx) = channel::bounded(0);
-    ctrlc::set_handler(move ||{
-        stx.send(()).expect("unable to send sigkill, you might as well panic!");
-    });
+    let (stx, srx) = channel::bounded(0);
+    ctrlc::set_handler(move || {
+        stx.send(())
+            .expect("unable to send sigkill, you might as well panic!");
+    })
+    .expect("unable to set SIGINT handler");
     p.run(srx);
 }
